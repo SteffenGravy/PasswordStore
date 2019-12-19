d11293276d4464deb7e4a584e822d61d706ac997
Steffen <Steffen.l@gmx.de>
2019-12-19 23:08:30 +0100

    Initial solution

---
+++ PasswordStore/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.IO;
+
+namespace PasswordStore
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        PasswordStoreData passwordStore = new PasswordStoreData();
+
+        public void program_Save(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+
+        public void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            if (saveFileDialog.ShowDialog() == true)
+                File.WriteAllBytes(saveFileDialog.FileName, passwordStore.toStore);
+        }
+
+        public void about_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Steffen L.", "Contributors");
+        }
+
+        public void program_Exit(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        public void program_Open(object sender, RoutedEventArgs e)
+        {
+            // TODO: open a file and load it
+        }
+
+        public void program_SaveAndExit(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ PasswordStore/PasswordStoreData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordStore
+{
+    public class PasswordStoreData
+    {
+        public byte[] toStore = new byte[] { };
+
+
+    }
+}
